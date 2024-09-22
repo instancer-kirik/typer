8788d7cc5fbc0943f3844e7cc0e7f320fea2f16b
instancer-kirik <106033356+instancer-kirik@users.noreply.github.com>
2024-09-21 23:34:00 -0400

    aaaa

--- lib/typer/release.ex
+++ lib/typer/release.ex
@@ -25,4 +25,26 @@ defmodule Typer.Release do
   defp load_app do
     Application.load(@app)
   end
+
+  def setup do
+    load_app()
+
+    for repo <- repos() do
+      case repo.__adapter__.storage_up(repo.config) do
+        :ok -> :ok
+        {:error, :already_up} -> :ok
+        {:error, term} -> {:error, term}
+      end
+    end
+
+    migrate()
+
+    Typer.Release.seed()
+  end
+
+  def seed do
+    # Add your seeding logic here
+    # For example:
+    # Typer.Repo.insert!(%Typer.SomeSchema{...})
+  end
 end
